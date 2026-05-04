1efa9293fec6565283e7f78f686ac49529edc42b
bawtek88 <bawtek88@gmail.com>
2026-05-04 09:11:39 +0200

    added docstring4

--- file1.py
+++ file1.py
@@ -1,4 +1,9 @@
+"""Module providing a function for getting current time."""
+
 import time
 
-for i in range(1,10):
-    print(time.time())
+
+def print_time():
+    """This prints time."""
+    for _ in range(1, 10):
+        print(time.time())
